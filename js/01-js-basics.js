fbfaaada85f1f25025d390a2d16774a04c815cda
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-12-12 18:53:05 +0530

    js basics

--- js/01-js-basics.js
+++ js/01-js-basics.js
@@ -29,24 +29,76 @@
 // console.log(num4);
 
 
-let someVariable = 10;
-console.log(typeof (someVariable));
-console.log(someVariable);
+// let someVariable = 10;
+// console.log(typeof (someVariable));
+// console.log(someVariable);
 
-someVariable = 12.75;
-console.log(typeof (someVariable));
-console.log(someVariable);
+// someVariable = 12.75;
+// console.log(typeof (someVariable));
+// console.log(someVariable);
 
-someVariable = 'abc';
-console.log(typeof (someVariable));
-console.log(someVariable);
+// someVariable = 'abc';
+// console.log(typeof (someVariable));
+// console.log(someVariable);
 
-someVariable = false;
-console.log(typeof (someVariable));
-console.log(someVariable);
+// someVariable = false;
+// console.log(typeof (someVariable));
+// console.log(someVariable);
 
-someVariable = 15.50;
-console.log(typeof (someVariable));
-console.log(someVariable);
+// someVariable = 15.50;
+// console.log(typeof (someVariable));
+// console.log(someVariable);
+
+// special Operators in JS 
+
+// const val1 = 10;
+// const val2 = '10';
+// console.log(val1 == val2);
+// console.log(val1 === val2); // type comparison operator 
+
+// console.log(val1 != val2);
+// console.log(val1 !== val2); // type comparison operator 
+
+
+// const num1 = 10;
+// const num2 = 'abc';
+// const num3 = num1 - num2;
+// console.log(num3);
+
+let num4 = null;
+console.log(typeof (num4));
+console.log(num4);
+
+num4 = 10;
+console.log(typeof (num4));
+console.log(num4);
+
+// truthy falsy values in JS 
+// falsy ->  false, 0, '', undefined, null 
+// truthy -> any other values 
+
+// please enter a value: 
+let val4;
+
+if (val4) {
+    console.log('truthy');
+}
+else {
+    console.log('falsy');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ js/02-js-functions.js
@@ -0,0 +1,24 @@
+
+// functions in JS 
+
+// 1. Old JS syntax 
+
+function fun1() {
+    console.log('fun1 function');
+}
+
+fun1();
+
+// 2. New JS syntax 
+
+const fun2 = () => {
+    console.log('fun2 function');
+}
+
+fun2();
+
+
+// JS PPTS - 
+// PPT1 - dont learn Scope (slide 31 onwards ) 
+// ppt2 - DONT LEARN HOISTING (slide )
+// PPT3 -  dont learn closure 
