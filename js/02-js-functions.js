76a88bd144359fa2bb1fd786c1b57f40cf71cf9b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-12-13 10:17:08 +0530

    js basics

--- js/02-js-functions.js
+++ js/02-js-functions.js
@@ -1,24 +1,30 @@
 
 // functions in JS 
 
-// 1. Old JS syntax 
+// // 1. Old JS syntax 
 
-function fun1() {
-    console.log('fun1 function');
-}
+// function fun1() {
+//     console.log('fun1 function');
+// }
 
-fun1();
+// fun1();
 
-// 2. New JS syntax 
+// // 2. New JS syntax 
 
-const fun2 = () => {
-    console.log('fun2 function');
-}
+// const fun2 = () => {
+//     console.log('fun2 function');
+// }
 
-fun2();
+// fun2();
 
 
 // JS PPTS - 
 // PPT1 - dont learn Scope (slide 31 onwards ) 
 // ppt2 - DONT LEARN HOISTING (slide )
 // PPT3 -  dont learn closure 
+
+const addNums = (a, b) => { return a + b };
+
+console.log(addNums(10, 20));
+
+
